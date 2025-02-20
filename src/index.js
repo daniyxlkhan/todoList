dbbeb66a9b698fe776daa1463d2220452b9d5dbe
Javantax <79591148+Javantax@users.noreply.github.com>
2025-02-19 19:26:32 -0500

    Add styles for a task and dialog js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import styles from "./style.css"
-import { openDialog, closeDialog } from "./create";
+import { openDialog, closeDialog } from "./showDialogForm";
 
 openDialog();
 closeDialog();
 
+
+const Projects = []
+
+const todoSubmitForm = document.querySelector("#todo-submit-form");
+
+function todo(title, description, dueDate, priority) {
+    return {
+        title,
+        description,
+        dueDate,
+        priority
+    };
+}
+
+function createTodo(title, description, dueDate, priority) {
+    const todoItem = todo(title, description, dueDate, priority);
+    console.log(todoItem);
+    Projects.push(todo);
+}
+
+todoSubmitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const description = document.querySelector("#details").value;
+    const dueDate = document.querySelector("#date").value;
+    const priority = document.querySelector(`input[name="priority"]:checked`)?.value;
+
+    createTodo(title, description, dueDate, priority);
+});
 
--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,38 @@
-
 const Projects = []
 
+const content = document.querySelector(".content");
+const todoSubmitForm = document.querySelector("#todo-submit-form");
+const homePage = document.querySelector(".home")
 function todo(title, description, dueDate, priority) {
     return {
         title,
-        description, 
-        dueDate, 
+        description,
+        dueDate,
         priority
     };
 }
+
+function createTodo(title, description, dueDate, priority) {
+    const todo = todo(title, description, dueDate, priority);
+    Projects.push(todo);
+}
+
+homePage.addEventListener("click", () => {
+    
+})
+
+todoSubmitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#title");
+    const description = document.querySelector("#details");
+    const dueDate = document.querySelector("#date");
+    const priority = document.querySelector(`input[name="priority"]:checked`)?.value;
+
+    const todo = todo(title, description, dueDate, priority);
+    console.log(todo);
+    return todo(title, description, dueDate, priority);
+})
+
+
 
